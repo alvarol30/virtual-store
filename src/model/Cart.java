f1e1bbc637ee9ec68c018fa7fc43d462a37eda59
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-05-23 18:41:18 -0500

    feat: Completé la clase Cart para que funcione con toda la logica del carrito de compras en una tienda

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -12,4 +12,21 @@ public class Cart{
         products = new ArrayList<>();
         discountStrategy = new NoDiscountStrategy();
     }
+    public void agregarProducto(Product product){
+        products.add(product);
+        System.out.println("Producto agregado: "+product.getName());
+    }
+    public void setDiscountStrategy(DiscountStrategy discountStrategy){
+        this.discountStrategy = discountStrategy;
+    }
+    public double calcularTotal(){
+        double total = 0;
+        for(Product product: products){
+            total = total + product.getPrice();
+        }
+        return total;
+    }
+    public double calcularTotalConDescuento(){
+        return discountStrategy.applyDiscount(calcularTotal());
+    }
 }
