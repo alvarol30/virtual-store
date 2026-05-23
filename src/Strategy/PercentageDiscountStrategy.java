77e431cd88569406cb5bceb040bef9bbe29e6fac
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-05-23 17:28:13 -0500

    feat: implementé la clase PercentageDiscountStrategy que hace lo mismo que en la clase de FixedAmountDiscountStrategy, solo que en vez de un monto fijo descuenta un monto porcentual y ya

--- src/Strategy/PercentageDiscountStrategy.java
+++ src/Strategy/PercentageDiscountStrategy.java
@@ -1,4 +1,11 @@
 package Strategy;
-
-public class PercentageDiscountStrategy {
+public class PercentageDiscountStrategy implements DiscountStrategy{
+    private float porcentaje;
+    public PercentageDiscountStrategy(float porcentaje){
+        this.porcentaje = porcentaje;
+    }
+    @Override
+    public double applyDiscount(double total){
+       return total - (total * porcentaje / 100);
+    }
 }
