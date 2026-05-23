0220e3ef6fba74ff6d06ba54890b7f7f7d5d5b98
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-05-23 17:07:14 -0500

    feat: implementé la lógica de la clase FixedAmountDiscountStrategy que solamente trata de descontar un monto fijo al MontoTotal que va en el carrito y de ahi sale en MontoFinal, que es lo que se cobrará al cliente

--- src/Strategy/FixedAmountDiscountStrategy.java
+++ src/Strategy/FixedAmountDiscountStrategy.java
@@ -1,4 +1,16 @@
 package Strategy;
-
-public class FixedAmountDiscountStrategy {
+public class FixedAmountDiscountStrategy implements DiscountStrategy{
+    private float MontoDescuento;
+    public FixedAmountDiscountStrategy(float MontoDescuento) {
+        this.MontoDescuento = MontoDescuento;
+    }
+    public double applyDiscount(double total) {
+        double MontoFinal= total - MontoDescuento;
+        if(MontoFinal < 0){
+            return 0;
+        }
+        else{
+            return MontoFinal;
+        }
+    }
 }
