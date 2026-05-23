eff243ca8ddbad91834a2e40a10e40a9f8a83ec8
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-05-23 18:54:25 -0500

    feat: implementé la clase OrderService con todo el proceso de compra y todo lo que saldrá en pantalla

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,4 +1,37 @@
 package service;
+import adapter.PaymentProcessor;
+import model.Cart;
+import observer.OrderObserver;
+import java.util.ArrayList;
+import java.util.List;
+public class OrderService{
+    private Cart cart;
+    private PaymentProcessor paymentProcessor;
+    private List<OrderObserver> observers;
 
-public class OrderService {
+    public OrderService(Cart cart, PaymentProcessor paymentProcessor){
+        this.cart = cart;
+        this.paymentProcessor = paymentProcessor;
+        this.observers = new ArrayList<>();
+    }
+    public void addObserver(OrderObserver observer){
+        observers.add(observer);
+    }
+    public void notifyObservers(String message){
+        for (OrderObserver observer : observers){
+            observer.update(message);
+        }
+    }
+    public void confirmOrder(){
+        double totalOriginal = cart.calcularTotal();
+        double totalFinal = cart.calcularTotalConDescuento();
+        System.out.println("\n========== RESUMEN ==========");
+        System.out.println("Total original: S/ " + totalOriginal);
+        System.out.println("Descuento Aplicado: -S/ " + (totalOriginal - totalFinal));
+        System.out.println("Total final: S/ " + totalFinal);
+        paymentProcessor.pay(totalFinal);
+        String message = "\nCompra confirmada por S/ " + totalFinal;
+        System.out.println(message);
+        notifyObservers(message);
+    }
 }
